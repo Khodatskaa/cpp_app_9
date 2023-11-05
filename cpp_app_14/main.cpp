38ade094545a7d6dc17108db31be15bcd53309d7
Khodatskaa <khodatskaalina@gmail.com>
2023-11-05 17:50:34 +0200

    Ex 2 was done

--- cpp_app_14/main.cpp
+++ cpp_app_14/main.cpp
@@ -4,23 +4,19 @@ using namespace std;
 
 int main()
 {
-	int num;
+    int num;
 
-	cout << "Enter a number: ";
-	cin >> num;
+    cout << "Enter a number: ";
+    cin >> num;
 
-	if (num > 0)
-	{
-		cout << "Number is positive" << endl;
-	}
-	else if (num < 0)
-	{
-		cout << "Number is negative" << endl;
-	}
-	else
-	{
-		cout << "Number is zero" << endl;
-	}
+    int *x = &num;
 
-	return 0;
+    if (*x > 0)
+        cout << "Number is positive" << endl;
+    else if (*x < 0)
+        cout << "Number is negative" << endl;
+    else
+        cout << "Number is zero" << endl;
+
+    return 0;
 }
